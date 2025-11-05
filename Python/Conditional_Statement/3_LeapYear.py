7aadf6486b0781810041298c91a22649c59fcd8e
Shaikh Parwez Alam <parwez.alam.0618@gmail.com>
2025-11-06 02:30:38 +0530

    Create 3_LeapYear.py

---
+++ Python/Conditional_Statement/3_LeapYear.py
@@ -0,0 +1,7 @@
+# check the year is leap year or not
+
+
+year = int(input("Enter Year : "))
+
+result = "Leap Year" if ((year % 4==0 and year % 100 !=0 ) or (year % 400 ==0)) else "Not a Leap Year"
+print(result)
